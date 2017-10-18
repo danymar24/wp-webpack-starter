d4739c7ad6a0badf30bc79350b72f01eb230e23d
Brian O'Toole <brian@sparxoo.com>
2017-10-18 13:58:02 -0400

    added custom post types include file

--- wp-content/themes/wp-boiler-webpack/functions.php
+++ wp-content/themes/wp-boiler-webpack/functions.php
@@ -91,6 +91,7 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/extras.php';
 require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/jetpack.php';
+require get_template_directory() . '/inc/custom-post-types.php';
 
 // Removes default WP junk from header
 remove_action('wp_head', 'rsd_link');
