7969a4a9cd0782e525151d8083b1baae0ea8ef7e
Brian O'Toole <brian@sparxoo.com>
2017-10-18 14:32:42 -0400

    added 404, search form, and styles

--- wp-content/themes/wp-boiler-webpack/404.php
+++ wp-content/themes/wp-boiler-webpack/404.php
@@ -1,64 +1,25 @@
 <?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package sparxoo-dev
- */
+get_header();
 
-get_header(); ?>
+get_template_part( 'template-parts/section', 'hero' );
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'sparxoo-dev' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'sparxoo-dev' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( sparxoo_dev_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'sparxoo-dev' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'sparxoo-dev' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<section class="section-wrap page-intro">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php
+          //while ( have_posts() ) : the_post();
+          //get_template_part( 'template-parts/content', 'page' );
+          //endwhile;
+        ?>
+        <p>The page you are looking for does not exist. <a href="<?php echo home_url(); ?>">Click here to return to homepage</a>, or try searching for another page using the form below.</p>
+        <?php get_template_part( 'template-parts/section', 'searchform' ); ?>
+      </div><!--/.col-->
+    </div><!--/.row-->
+  </div><!--/.container-->
+</section>
 
 <?php
 get_footer();
+?>
