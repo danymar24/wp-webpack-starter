88e6c848a8b8f6a0edf8d4ec0ab0f83137384417
Daniel M Rodriguez (TMS) <daniel.m.rodriguez@toyota.com>
2019-03-22 14:29:42 -0500

    Added contact field group

--- wp-content/themes/wp-boiler-webpack/template-parts/content-page.php
+++ wp-content/themes/wp-boiler-webpack/template-parts/content-page.php
@@ -1,43 +1,14 @@
-<?php
-/**
- * Template part for displaying page content in page.php
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package sparxoo-dev
- */
-
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<div class="row">
+	<div class="col-md-12">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	</div>
+</div>
+<div class="row" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-		<?php
-			the_content();
+	<div class="col-md-12">
+		<?php while ( have_posts() ) : the_post();
+			the_content(); 
+		endwhile ?>
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sparxoo-dev' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'sparxoo-dev' ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-## -->
+	</div>
+</div>
--- wp-content/themes/wp-boiler-webpack/templates/page-contact.php
+++ wp-content/themes/wp-boiler-webpack/templates/page-contact.php
@@ -1,13 +1,25 @@
 <?php
     /*
     Template Name: Contact
+    Field group: Page - Contact
     */
 
     get_header();
 
-    get_template_part( 'template-parts/section', 'hero' );
+    get_template_part( 'template-parts/section', 'hero' ); 
     get_template_part( 'template-parts/content', 'page' );
-
+?>
+    
+    <div class="row">
+        <div class="col-md-6"><?php the_field('address') ?></div>
+        <div class="col-md-6"><?php the_field('map') ?></div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <?php the_field('form'); ?>
+        </div>
+    </div>
+    <?php
     get_footer();
 
 ?>
